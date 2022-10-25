7b6c7fb3ed1b3cdf161376478d40e4fc30341d3f
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-10-25 07:34:30 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello tailwind</h1>
+    <div className="">
+      <h1 className="text-2xl text-center text-teal-500">hello tailwind</h1>
+      <h2>helooo</h2>
+
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
